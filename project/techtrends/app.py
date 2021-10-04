869d1b514a5c18163b944c600b54d978ffcd5fa5
Eviliana Tsamadou <evtsamadou@gmail.com>
2021-10-05 00:06:28 +0300

    Format file with pycharm styling rules

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -6,56 +6,64 @@ from werkzeug.exceptions import abort
 # Global scope variable
 connection_count = 0
 
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
-    connection = sqlite3.connect('database.db')
-    connection.row_factory = sqlite3.Row
-    global connection_count
-    connection_count = connection_count + 1
-    return connection
+	connection = sqlite3.connect('database.db')
+	connection.row_factory = sqlite3.Row
+	global connection_count
+	connection_count = connection_count + 1
+	return connection
+
 
 # Function to get a post using its ID
 def get_post(post_id):
-    connection = get_db_connection()
-    post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    connection.close()
-    return post
+	connection = get_db_connection()
+	post = connection.execute('SELECT * FROM posts WHERE id = ?',
+							  (post_id,)).fetchone()
+	connection.close()
+	return post
+
 
 # Function to get total amount of posts in the database
 def get_total_posts():
-    connection = get_db_connection()
-    total_posts = connection.execute('SELECT * FROM posts').fetchall()
-    connection.close()
-    return len(total_posts)
+	connection = get_db_connection()
+	total_posts = connection.execute('SELECT * FROM posts').fetchall()
+	connection.close()
+	return len(total_posts)
+
 
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
+
 # Define the main route of the web application
 @app.route('/')
 def index():
-    connection = get_db_connection()
-    posts = connection.execute('SELECT * FROM posts').fetchall()
-    connection.close()
-    return render_template('index.html', posts=posts)
+	connection = get_db_connection()
+	posts = connection.execute('SELECT * FROM posts').fetchall()
+	connection.close()
+	return render_template('index.html', posts=posts)
+
 
 # Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
-    post = get_post(post_id)
-    if post is None:
-      return render_template('404.html'), 404
-    else:
-      return render_template('post.html', post=post)
+	post = get_post(post_id)
+	if post is None:
+		return render_template('404.html'), 404
+	else:
+		return render_template('post.html', post=post)
+
 
 # Define the About Us page
 @app.route('/about')
 def about():
-    return render_template('about.html')
+	return render_template('about.html')
+
 
 # Define the healtz endpoint
 @app.route('/healthz')
@@ -82,26 +90,28 @@ def metrics():
 	)
 	return response
 
+
 # Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
+	if request.method == 'POST':
+		title = request.form['title']
+		content = request.form['content']
 
-        if not title:
-            flash('Title is required!')
-        else:
-            connection = get_db_connection()
-            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
-            connection.commit()
-            connection.close()
+		if not title:
+			flash('Title is required!')
+		else:
+			connection = get_db_connection()
+			connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+							   (title, content))
+			connection.commit()
+			connection.close()
 
-            return redirect(url_for('index'))
+			return redirect(url_for('index'))
 
-    return render_template('create.html')
+	return render_template('create.html')
+
 
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+	app.run(host='0.0.0.0', port='3111')
